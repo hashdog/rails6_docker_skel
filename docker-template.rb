47ddfe9e6e014bde199f7624dfbc1d6c2db2b074
chebyte <chebyte@protonmail.com>
2020-05-07 19:57:25 -0300

    Initial commit

--- docker-template.rb
+++ docker-template.rb
@@ -18,15 +18,3 @@ append_file '.gitignore', <<-TXT
 .Procfile
 .env
 TXT
-
-# append_file 'env-sample', <<-TXT
-# RAILS_PORT=3000
-# DATABASE_URL=postgresql://postgres@db
-# RAILS_ENV=development
-# WEBPACKER_DEV_SERVER_HOST=0.0.0.0
-# NODE_ENV=development
-# WEB_CONCURRENCY=0
-# SECRET_KEY_BASE=5e1e813f2f5b3a7493c2e2451e887c930d2edc978aec7061ab7823d0b126f28e9c80d14b3858419fd4e805ddb038188bd6ffa58f2cc5112eeb33b9dca764f973
-# TXT
-
-# run 'cp env-sample .env'
