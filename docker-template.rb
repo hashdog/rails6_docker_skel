2121dcfc69a8468cee53ac9ebd3dbf9161f0e15c
chebyte <chebyte@protonmail.com>
2020-05-07 19:57:08 -0300

    Initial commit

--- docker-template.rb
+++ docker-template.rb
@@ -8,7 +8,7 @@ run 'curl -L https://github.com/hashdog/rails6_docker_skel/archive/master.zip > 
 run 'unzip docker-skel.zip && cp -af rails6_docker_skel-master/* . && rm -f docker-template.rb && rm -rf rails6_docker_skel-master && rm docker-skel.zip'
 run 'cp docker-compose.override.yml-sample docker-compose.override.yml'
 run 'cp Procfile-sample Procfile'
-#run 'cp env-sample .env'
+run 'cp env-sample .env'
 
 # Git ignore
 ########################################
@@ -19,14 +19,14 @@ append_file '.gitignore', <<-TXT
 .env
 TXT
 
-append_file 'env-sample', <<-TXT
-RAILS_PORT=3000
-DATABASE_URL=postgresql://postgres@db
-RAILS_ENV=development
-WEBPACKER_DEV_SERVER_HOST=0.0.0.0
-NODE_ENV=development
-WEB_CONCURRENCY=0
-SECRET_KEY_BASE=5e1e813f2f5b3a7493c2e2451e887c930d2edc978aec7061ab7823d0b126f28e9c80d14b3858419fd4e805ddb038188bd6ffa58f2cc5112eeb33b9dca764f973
-TXT
+# append_file 'env-sample', <<-TXT
+# RAILS_PORT=3000
+# DATABASE_URL=postgresql://postgres@db
+# RAILS_ENV=development
+# WEBPACKER_DEV_SERVER_HOST=0.0.0.0
+# NODE_ENV=development
+# WEB_CONCURRENCY=0
+# SECRET_KEY_BASE=5e1e813f2f5b3a7493c2e2451e887c930d2edc978aec7061ab7823d0b126f28e9c80d14b3858419fd4e805ddb038188bd6ffa58f2cc5112eeb33b9dca764f973
+# TXT
 
-run 'cp env-sample .env'
+# run 'cp env-sample .env'
